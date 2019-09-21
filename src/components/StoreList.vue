eff3d47c902bd12ae226f7a79b647cceb1a05808
chunyayang <chunya.yg@gmail.com>
2019-09-21 23:51:52 +0800

    reformat

--- src/components/StoreList.vue
+++ src/components/StoreList.vue
@@ -1,59 +1,33 @@
-<template> 
+<template>
   <div class="store-list">
     <v-alert
       v-if="errored"
-      type="error"      
-    >
-      We're sorry, we're not able to retrieve this information at the moment, please try back later
-    </v-alert>
+      type="error"
+    >We're sorry, we're not able to retrieve this information at the moment, please try back later</v-alert>
 
-    <ul
-      v-else
-      class="d-flex flex-column flex-sm-row list"
-    >
-      <li
-        v-for="store in stores"
-        :key="store.id"
-        class="flex sm6 px-sm-2"
-      >
+    <ul v-else class="d-flex flex-column flex-sm-row list">
+      <li v-for="store in stores" :key="store.id" class="flex sm6 px-sm-2">
         <v-card flat>
-          <v-img
-            :src="require('../assets/' + store.imageUrl)"
-            aspect-ratio="2"
-          ></v-img>
+          <v-img :src="require('../assets/' + store.imageUrl)" aspect-ratio="2"></v-img>
 
-          <v-card-title class="subtitle-1 pb-0">
-            {{ store.name }}
-          </v-card-title>
+          <v-card-title class="subtitle-1 pb-0">{{ store.name }}</v-card-title>
 
           <v-card-text>
             <div>
               <span class="pr-1">
-                <v-icon
-                  color="orange"
-                  small
-                >
-                  {{ svgStar }}
-                </v-icon>
+                <v-icon color="orange" small>{{ svgStar }}</v-icon>
               </span>
               <span class="pr-1 font-weight-medium">{{ store.rating }}</span>
               <span>({{ store.ratingCount }})&nbsp;</span>
-              <span
-                v-for="tag in store.tags"
-                :key="tag"
-                class="tag"
-              >
-                {{ tag }}
-              </span>
+              <span v-for="tag in store.tags" :key="tag" class="tag">{{ tag }}</span>
             </div>
 
             <div>
               <span>{{ store.distance }}</span>
-              <span>&nbsp;·&nbsp;</span>
+              <span>・</span>
               <span>{{ formatPriceLevel(store.priceLevel) }}</span>
-            </div>            
+            </div>
           </v-card-text>
-
         </v-card>
       </li>
     </ul>
@@ -61,30 +35,30 @@
 </template>
 
 <script>
-import { getStores } from '@/api.js'
-import { mdiStar } from '@mdi/js'
-import storeMixin from "../mixins/storeMixin"
+import { getStores } from "@/api.js";
+import { mdiStar } from "@mdi/js";
+import storeMixin from "../mixins/storeMixin";
 
 export default {
-  data () {
+  data() {
     return {
       stores: [],
       svgStar: mdiStar,
-      errored: false      
-    }
+      errored: false
+    };
   },
-  mounted () {
+  mounted() {
     getStores()
-    .then(response => {
-      this.stores = response.data
-    })
-    .catch(error => {
-      console.log(error)
-      this.errored = true
-    })
+      .then(response => {
+        this.stores = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+        this.errored = true;
+      });
   },
   mixins: [storeMixin]
-}
+};
 </script>
 
 <style lang="scss">
@@ -92,10 +66,10 @@ export default {
   list-style-type: none;
   padding: 0 !important;
 }
-.store-list .tag::after{
-  content: '·';
+.store-list .tag::after {
+  content: "・";
 }
 .store-list .tag:last-child::after {
-  content: '';
+  content: "";
 }
 </style>>
