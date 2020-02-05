0afcb36d720354502b5d14532d069076bcf725c7
chunyayang <chunya.yg@gmail.com>
2020-02-05 20:35:55 +0800

    tech: fix the coding style

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import Vue from 'vue'
+import Vue from 'vue';
 import vuetify from './plugins/vuetify';
-import router from './router'
-import App from './App.vue'
+import router from './router';
+import App from './App.vue';
 
 var EventBus = new Vue();
 
